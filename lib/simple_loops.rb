ec00a64126903568eff07486806bda2ce860102c
Ziad Zebib <zebibziad@gmail.com>
2020-12-22 19:07:20 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -6,3 +6,12 @@ def loop_message_five_times(message)
     counter += 1 
   end
 end 
+
+def loop_message_five_times(message)
+  counter = 0
+  
+  while counter < 5 do
+    puts message
+    counter += 1 
+  end
+end 
